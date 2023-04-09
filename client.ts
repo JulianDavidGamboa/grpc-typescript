a88293036716eaaa5722e70540691f9a00994d41
Julian Gamboa <juliandg34@gmail.com>
2023-04-09 02:04:57 -0500

    Adding a chat app

--- client.ts
+++ client.ts
@@ -3,6 +3,7 @@ import * as grpc from '@grpc/grpc-js';
 import * as protoloader from '@grpc/proto-loader';
 import { ProtoGrpcType } from './proto/random';
 import { RandomHandlers } from './proto/randomPackage/Random';
+import readline from 'readline';
 
 const PORT = 8082;
 const PROTO_FILE = './proto/random.proto';
@@ -41,17 +42,47 @@ function onClientReady() {
     //     console.log("communication ended");
     // })
 
-    const stream = client.TodoList((err, result) => {
-        if(err) {
-            console.error(err);
-            return;
-        }
-        console.log(result);
+    // const stream = client.TodoList((err, result) => {
+    //     if(err) {
+    //         console.error(err);
+    //         return;
+    //     }
+    //     console.log(result);
+    // });
+    // stream.write({todo: "Walk the dog", status: "Never"})
+    // stream.write({todo: "Walk the wife", status: "Doing"})
+    // stream.write({todo: "Get a real job", status: "Impossible"})
+    // stream.write({todo: "Feed the dog", status: "Done"})
+    // stream.end();
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
     });
-    stream.write({todo: "Walk the dog", status: "Never"})
-    stream.write({todo: "Walk the wife", status: "Doing"})
-    stream.write({todo: "Get a real job", status: "Impossible"})
-    stream.write({todo: "Feed the dog", status: "Done"})
-    stream.end();
+
+    const username = process.argv[2];
+    if (!username) console.error("no username, can't join chat"), process.exit();
+
+    const metadata = new grpc.Metadata();
+    metadata.set("username", username);
+    const call = client.Chat(metadata);
+
+    call.write({
+        message: "register"
+    });
+
+    call.on("data", (chunk) => {
+        console.log(`${chunk.username} ==> ${chunk.message}`)
+    })
+
+    rl.on("line", (line) => {
+        if(line === "quit") {
+            call.end();
+        } else {
+            call.write({
+                message: line
+            })
+        }
+    })
 }
 
